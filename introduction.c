56dcc4530be13b9deeaf77dd785d9fb3bfeeab2b
Aayus Regmi <aayusregmi721@gmail.com>
2026-05-06 23:31:14 +0545

    nothing to do

---
+++ introduction.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    Printf("<------Introduction-------->");
+    printf("Name : Aayus Regmi \t | University : Patan Multiple Campus \t | Passionate");
+    return 0;
+}
